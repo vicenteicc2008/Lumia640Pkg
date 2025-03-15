c2edbc697c7e88828a8bfe0a52c34f8f663db843
Vicente Cortes <vicenteicc2008@gmail.com>
2025-03-15 11:16:23 -0300

    Revert "boot straight to Windows"
    this reverts commit https://github.com/vicenteicc2008/Lumia640Pkg/commit/ad96e078fb0bac3cbb685fa42e461ff6f28b9b94

--- Library/PlatformBootManagerLib/PlatformBm.c
+++ Library/PlatformBootManagerLib/PlatformBm.c
@@ -772,7 +772,7 @@ PlatformBootManagerAfterConsole (
   //
   // Enumerate all possible boot options.
   //
-  EfiBootManagerRefreshAllBootOption ();
+  // EfiBootManagerRefreshAllBootOption ();
 
   //
   // Register UEFI Shell
